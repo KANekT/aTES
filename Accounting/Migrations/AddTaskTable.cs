9696b19c86441c6b9e7bc60f7fdf1603491a5eab
Pronin Andrey <mail@innoline.pro>
2024-03-03 08:56:00 +0300

    + accounting

---
+++ Accounting/Migrations/AddTaskTable.cs
@@ -0,0 +1,43 @@
+using Core.Enums;
+using FluentMigrator;
+
+namespace Accounting.Migrations;
+
+[Migration(2)]
+public class AddTaskTable : Migration
+{
+    public override void Up()
+    {
+        Create.Table("Tasks")
+            .WithColumn(nameof(TaskDto.Id)).AsInt64().PrimaryKey().Identity()
+            .WithColumn(nameof(TaskDto.Ulid)).AsString().NotNullable()
+            .WithColumn(nameof(TaskDto.CreatedAt)).AsDateTime().WithDefault(SystemMethods.CurrentUTCDateTime)
+            .WithColumn(nameof(TaskDto.EditedAt)).AsDateTime().WithDefault(SystemMethods.CurrentUTCDateTime)
+            .WithColumn(nameof(TaskDto.Description)).AsString().NotNullable()
+            .WithColumn(nameof(TaskDto.Status)).AsByte().NotNullable().WithDefaultValue((byte)StatusEnum.Open)
+            //.WithColumn(nameof(TaskDto.Lose)).AsInt32().NotNullable()
+            //.WithColumn(nameof(TaskDto.Reward)).AsInt32().NotNullable()
+            .WithColumn(nameof(TaskDto.PoPugId)).AsString().NotNullable()
+            ;
+
+        /*
+        for (var i = 0; i < 50; i++)
+        {
+            var task = new Dictionary<string, object>
+            {
+                { nameof(TaskDto.Description), "Task # " + Random.Shared.Next(0, 500) },
+                { nameof(TaskDto.Lose), Random.Shared.Next(10, 20) },
+                { nameof(TaskDto.Reward), Random.Shared.Next(20, 40) },
+                { nameof(TaskDto.PoPugId), Random.Shared.Next(1, 25) }
+            };
+            
+            Insert.IntoTable(nameof(Task)).Row(task);
+        }
+        */
+    }
+
+    public override void Down()
+    {
+        Delete.Table(nameof(TaskDto));
+    }
+}
